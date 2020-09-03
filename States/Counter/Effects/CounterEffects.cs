0c89dda2854e506721c57cedfb18b74d391ca4b5
Chris <christoment@gmail.com>
2020-09-03 14:13:26 +1000

    Effects with injection

--- States/Counter/Effects/CounterEffects.cs
+++ States/Counter/Effects/CounterEffects.cs
@@ -7,12 +7,18 @@ namespace BlazorWithRedux.States.Counter.Effects
 {
     public class CounterEffects
     {
+        private readonly IState<CounterState> counterState;
+
         // Can use injection
+        public CounterEffects(IState<CounterState> counterState)
+        {
+            this.counterState = counterState;
+        }
 
         [EffectMethod]
         public Task LogIncreaseCounter(IncreaseCounter action, IDispatcher dispatcher)
         {
-            Console.WriteLine($"Counter increased with step {action.Step}");
+            Console.WriteLine($"Current count: {counterState.Value.Count}. Counter increased with step {action.Step}");
 
             return Task.CompletedTask;
         }
