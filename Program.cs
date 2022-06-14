385f9a3380c02a85b9804586a6fa8fe483de87e0
Chris <christoment@gmail.com>
2022-06-14 12:54:12 +1000

    Update to use .NET 6 minimal hosting model

--- Program.cs
+++ Program.cs
@@ -1,33 +1,22 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using BlazorWithRedux;
 using Fluxor;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace BlazorWithRedux
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFluxor(options =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+    options
+        .ScanAssemblies(typeof(Program).Assembly)
+        .UseReduxDevTools();
+});
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            builder.Services.AddFluxor(options =>
-            {
-                options
-                    .ScanAssemblies(typeof(Program).Assembly)
-                    .UseReduxDevTools();
-            });
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
